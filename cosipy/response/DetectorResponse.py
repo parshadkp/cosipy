ea4d0f5afa4addfe653008885a5db6e8cb4ee41a
parshadkp <111451184+parshadkp@users.noreply.github.com>
2025-09-08 22:53:57 -0400

    Issue with source injector code.

--- cosipy/response/DetectorResponse.py
+++ cosipy/response/DetectorResponse.py
@@ -3,7 +3,9 @@ logger = logging.getLogger(__name__)
 
 import numpy as np
 
-from histpy import Histogram
+from copy import deepcopy
+
+from histpy import Histogram, Axes, Axis
 
 import astropy.units as u
 
@@ -35,31 +37,7 @@ class DetectorResponse(Histogram):
 
         self._spec = None
         self._aeff = None
-
-    def copy(self):
-
-        new = super().copy()
-
-        new._spec = None if self._spec is None else self._spec.copy()
-        new._aeff = None if self._aeff is None else self._aeff.copy()
-
-        return new
-
-    def _write(self, file, group_name):
-        group = super()._write(file, group_name)
-
-        # do not write _spec and _aeff, as they
-        # can be recomputed after load later on
-
-    @classmethod
-    def _open(cls, hist_group):
-        new = super()._open(hist_group)
-
-        new._spec = None
-        new._aeff = None
-
-        return new
-
+    
     def get_spectral_response(self, copy = True):
         """
         Reduced detector response, projected along the real and measured energy axes only.
@@ -69,7 +47,7 @@ class DetectorResponse(Histogram):
         ----------
         copy : bool
             If true, a copy of the cached spectral response will be returned.
-
+        
         Returns
         -------
         :py:class:`DetectorResponse`
@@ -80,14 +58,13 @@ class DetectorResponse(Histogram):
             spec = self.project(['Ei','Em'])
             self._spec = DetectorResponse(spec.axes,
                                           contents = spec.contents,
-                                          unit = spec.unit,
-                                          copy_contents = False)
+                                          unit = spec.unit)
 
         if copy:
-            return self._spec.copy()
+            return deepcopy(self._spec)
         else:
             return self._spec
-
+        
     def get_effective_area(self, energy = None, copy = True):
         """
         Compute the effective area at a given energy. If no energy is specified, the
@@ -99,18 +76,18 @@ class DetectorResponse(Histogram):
             Energy/energies at which to interpolate the linearly effective area
         copy : bool
             If true, a copy of the cached effective will be returned.
-
+        
         Returns
         -------
         :py:class:`astropy.units.Quantity` or :py:class:`histpy.Histogram`
         """
-
+        
         if self._aeff is None:
-            self._aeff = self.get_spectral_response(copy = False).project('Ei')
+            self._aeff = self.get_spectral_response(copy = False).project('Ei').to_dense()
 
         if energy is None:
             if copy:
-                return self._aeff.copy()
+                return deepcopy(self._aeff)
             else:
                 return self._aeff
         else:
@@ -126,7 +103,7 @@ class DetectorResponse(Histogram):
         -------
         :py:class:`histpy.Histogram`
         """
-
+        
         # Get spectral response and effective area normalization
         spec = self.get_spectral_response(copy = False)
         norm = self.get_effective_area().full_contents
@@ -140,10 +117,10 @@ class DetectorResponse(Histogram):
             norm[norm == 0] = 1*norm.unit
 
             logger.warn("Null effective area, cannot properly compute dispersion matrix.")
-
+        
         # "Broadcast" such that it has the compatible dimensions with the 2D matrix
         norm = spec.expand_dims(norm, 'Ei')
-
+        
         # Normalize column-by-column
         return (spec / norm)
 
@@ -156,10 +133,10 @@ class DetectorResponse(Histogram):
         -------
         :py:class:`histpy.Axes`
         """
-
+        
         return self.axes['Ei']
 
-
+    
     @property
     def measured_energy_axis(self):
         """
@@ -167,7 +144,11 @@ class DetectorResponse(Histogram):
 
         Returns
         -------
-        :py:class:`histpy.Axes`
+        :py:class:`histpy.Axes`        
         """
-
+        
         return self.axes['Em']
+        
+
+        
+    
